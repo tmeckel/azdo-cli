af57b76e1a5291e4d854ac2bc94414f9ed6a7712
Thomas Meckel <tmeckel@users.noreply.github.com>
2023-08-11 13:40:22 +0000

    Implemented auth logout Fixes: #3

--- internal/cmd/auth/auth.go
+++ internal/cmd/auth/auth.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/tmeckel/azdo-cli/internal/cmd/auth/gitcredential"
 	"github.com/tmeckel/azdo-cli/internal/cmd/auth/login"
+	"github.com/tmeckel/azdo-cli/internal/cmd/auth/logout"
 	"github.com/tmeckel/azdo-cli/internal/cmd/auth/setupgit"
 	"github.com/tmeckel/azdo-cli/internal/cmd/auth/status"
 	"github.com/tmeckel/azdo-cli/internal/cmd/util"
@@ -20,7 +21,7 @@ func NewCmdAuth(ctx util.CmdContext) *cobra.Command {
 
 	cmd.AddCommand(gitcredential.NewCmdGitCredential(ctx))
 	cmd.AddCommand(login.NewCmdLogin(ctx))
-	// cmd.AddCommand(logout.NewCmdLogout(ctx))
+	cmd.AddCommand(logout.NewCmdLogout(ctx))
 	cmd.AddCommand(status.NewCmdStatus(ctx))
 	cmd.AddCommand(setupgit.NewCmdSetupGit(ctx))
 
