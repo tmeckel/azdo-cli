53b628f513b0d0095fec90f94c03f45a643ebcfe
Thomas Meckel <tmeckel@users.noreply.github.com>
2025-09-06 19:17:01 +0000

    feat(pr): ✨ add PR command group

---
+++ internal/cmd/pr/pr.go
@@ -0,0 +1,57 @@
+package pr
+
+import (
+	"github.com/MakeNowJust/heredoc"
+	"github.com/spf13/cobra"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/checkout"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/close"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/comment"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/create"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/diff"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/edit"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/list"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/merge"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/reopen"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/status"
+	"github.com/tmeckel/azdo-cli/internal/cmd/pr/view"
+	"github.com/tmeckel/azdo-cli/internal/cmd/util"
+)
+
+func NewCmdPR(ctx util.CmdContext) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "pr <command>",
+		Short: "Manage pull requests",
+		Long:  "Work with Azure DevOps pull requests.",
+		Example: heredoc.Doc(`
+			$ azdo pr checkout 353
+			$ azdo pr create --fill
+		`),
+		Annotations: map[string]string{
+			"help:arguments": heredoc.Doc(`
+				A pull request can be supplied as argument in any of the following formats:
+				- by number, e.g. "123";
+				- by the name of its head branch, e.g. "patch-1" or "OWNER:patch-1".
+			`),
+		},
+		GroupID: "core",
+	}
+
+	util.AddGroup(cmd, "General commands",
+		list.NewCmd(ctx),
+		create.NewCmd(ctx),
+		status.NewCmd(ctx),
+	)
+
+	util.AddGroup(cmd, "Targeted commands",
+		checkout.NewCmd(ctx),
+		close.NewCmd(ctx),
+		comment.NewCmd(ctx),
+		diff.NewCmd(ctx),
+		edit.NewCmd(ctx),
+		merge.NewCmd(ctx),
+		reopen.NewCmd(ctx),
+		view.NewCmd(ctx),
+	)
+
+	return cmd
+}
