043bb84e09c41c9754f2a7a9ddf7bd448de1cae2
Thomas Meckel <tmeckel@users.noreply.github.com>
2025-09-21 17:30:28 +0000

    refactor: ⚙️ moved util internal/connection_test.go to internal/azdo/adapter_test.go

--- internal/azdo/adapter_test.go
+++ internal/azdo/adapter_test.go
@@ -1,29 +1,22 @@
-package util_test
+package azdo_test
 
 import (
 	"testing"
 
-	"github.com/microsoft/azure-devops-go-api/azuredevops/v7"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"github.com/tmeckel/azdo-cli/internal/util"
+	"github.com/tmeckel/azdo-cli/internal/azdo"
 )
 
 func TestOrganizationFromConnection(t *testing.T) {
-	var conn *azuredevops.Connection
-
 	for _, url := range []string{
 		"https://dev.azure.com/org1",
 		"https://org1.visualstudio.com",
 		"https://org1.visualstudio.com/long/path/89?query=1",
 		"https://dev.azure.com/org1/long/path/89?query=1",
 	} {
-		conn = &azuredevops.Connection{
-			BaseUrl: url,
-		}
+		conn := azdo.NewPatConnection(url, "pat")
 
-		org, err := util.GetOrganizationFromConnection(conn)
-		require.NoError(t, err)
+		org := conn.Organization()
 		assert.Equal(t, "org1", org)
 	}
 }
