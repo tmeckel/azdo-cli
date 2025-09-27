3ec0f36404d3585f31c7e440b803bac54866d27c
Thomas Meckel <tmeckel@users.noreply.github.com>
2025-09-27 17:26:11 +0000

    refactor(util/auth): ♻️ standardize authentication helper routines

--- internal/cmd/util/auth.go
+++ internal/cmd/util/auth.go
@@ -2,26 +2,27 @@ package util
 
 import (
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v7"
+	"github.com/tmeckel/azdo-cli/internal/azdo"
 	"github.com/tmeckel/azdo-cli/internal/config"
 )
-
-type Authenticator interface {
-	GetAuthorizationHeader(organizationName string) (string, error)
-}
 
 type patAuthenticator struct {
 	cfg config.Config
 }
 
-func NewPatAuthenticator(cfg config.Config) (instance Authenticator, err error) {
+func NewPatAuthenticator(cfg config.Config) (instance azdo.Authenticator, err error) {
 	instance = &patAuthenticator{
 		cfg: cfg,
 	}
 	return instance, err
 }
 
+func (p *patAuthenticator) GetPAT(organizationName string) (string, error) {
+	return p.cfg.Authentication().GetToken(organizationName)
+}
+
 func (p *patAuthenticator) GetAuthorizationHeader(organizationName string) (hdrValue string, err error) {
-	pat, err := p.cfg.Authentication().GetToken(organizationName)
+	pat, err := p.GetPAT(organizationName)
 	if err != nil {
 		return hdrValue, err
 	}
