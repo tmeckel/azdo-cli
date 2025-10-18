4c1d7052675fa1dcdc5783510276abe58d53ac1b
Thomas Meckel <tmeckel@users.noreply.github.com>
2025-10-18 21:35:30 +0000

    feat(security/group/membership): ✨ add add/remove membership\n\nIntegrated identities and confirmation for destructive actions.

---
+++ internal/cmd/security/group/membership/membership.go
@@ -0,0 +1,18 @@
+package membership
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/tmeckel/azdo-cli/internal/cmd/security/group/membership/list"
+	"github.com/tmeckel/azdo-cli/internal/cmd/util"
+)
+
+func NewCmd(ctx util.CmdContext) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "membership",
+		Short:   "Manage security group memberships",
+		Aliases: []string{"m"},
+	}
+
+	cmd.AddCommand(list.NewCmd(ctx))
+	return cmd
+}
